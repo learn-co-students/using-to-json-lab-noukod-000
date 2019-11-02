2b3eaad45b991cd6765ca6e26b354565db76271c
POLIARD DJimmy <poliarddjimmy@gmail.com>
2019-11-02 02:26:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
   get '/products/:id/data', to: 'products#data'
+
+  root 'products#index'
 end
